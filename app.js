6b4a7f7e4c7b17a55a96c94b6d317b27215db67c
DesperadoUa <Lazarev-konstant@mail.ru>
2024-04-01 17:18:59 +0300

    add express for api

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { API_URL, API_KEY } = require('./secret.js')
+const fetch = require('node-fetch')
+const app = express().use(bodyParser.json())
+
+let cash = {
+	data: []
+}
+const url = `${API_URL}recruitment/vacancies?page=1`
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/x-www-form-urlencoded',
+		'X-API-KEY': API_KEY
+	}
+}
+async function fn(indexPage) {
+	const url = `${API_URL}recruitment/vacancies?page=${indexPage}`
+	const res = await fetch(url, options)
+	const { data } = await res.json()
+	return data
+}
+async function init() {
+	const res = await fetch(url, options)
+	const { data, metadata } = await res.json()
+	const pages = metadata.pagination.pages
+	let store = data
+	const arr = new Array(parseInt(pages))
+	for await (let [index, value] of arr.entries()) {
+		if (index > 0) {
+			const res = await fn(index)
+			store = store.concat(res)
+		}
+	}
+	const posts = store.filter(item => item.state === 'opened')
+	return posts
+}
+app.get('/', async (req, res) => {
+	const res = await init()
+	res.json(res)
+})
+app.listen(3000, () => console.log('[People force api] is listening'))
